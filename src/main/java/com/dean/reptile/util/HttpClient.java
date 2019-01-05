bc1f47c6b221fa9184ab8ce30327e2e5787b64ed
dean <ladeanrobin@163.com>
2019-01-06 00:54:02 +0800

    优化httpClient

--- src/main/java/com/dean/reptile/util/HttpClient.java
+++ src/main/java/com/dean/reptile/util/HttpClient.java
@@ -47,37 +47,37 @@ public class HttpClient {
 
     static {
         // 以后把代理IP配置到数据库
-        //map.put("47.96.136.190", "8118");
-        //map.put("117.90.5.192", "9000");
-        //map.put("112.16.28.103", "8060");
-        //map.put("180.104.63.78", "9000");
-        //map.put("139.196.137.255", "8118");
+        map.put("47.96.136.190", "8118");
+        map.put("117.90.5.192", "9000");
+        map.put("112.16.28.103", "8060");
+        map.put("180.104.63.78", "9000");
+        map.put("139.196.137.255", "8118");
 
-        //map.put("183.230.177.118", "8060");
-        //map.put("115.231.5.230", "44524");
-        //map.put("182.61.59.147", "9999");
-        //map.put("119.145.136.126", "8888");
-        //map.put("123.56.86.158", "8080");
-        //
-        //map.put("118.178.227.171", "80");
-        //map.put("182.111.64.8", "53364");
-        //map.put("47.95.213.117", "80");
-        //map.put("183.129.207.84", "33555");
-        //map.put("115.218.221.25", "9000");
-        //
-        //map.put("60.171.111.113", "33069");
-        //map.put("115.223.211.4", "9000");
-        //map.put("49.51.68.122", "1080");
-        //map.put("112.25.129.174", "41323");
-        //map.put("140.207.25.114", "50750");
-        //
-        //map.put("218.198.117.194", "39248");
-        //map.put("117.131.75.134", "80");
-        //map.put("121.8.98.196", "80");
-        //map.put("221.7.255.168", "8080");
-        //map.put("120.76.77.152", "9999");
-        //
-        //map.put("117.87.177.58", "9000");
+        map.put("183.230.177.118", "8060");
+        map.put("115.231.5.230", "44524");
+        map.put("182.61.59.147", "9999");
+        map.put("119.145.136.126", "8888");
+        map.put("123.56.86.158", "8080");
+
+        map.put("118.178.227.171", "80");
+        map.put("182.111.64.8", "53364");
+        map.put("47.95.213.117", "80");
+        map.put("183.129.207.84", "33555");
+        map.put("115.218.221.25", "9000");
+
+        map.put("60.171.111.113", "33069");
+        map.put("115.223.211.4", "9000");
+        map.put("49.51.68.122", "1080");
+        map.put("112.25.129.174", "41323");
+        map.put("140.207.25.114", "50750");
+
+        map.put("218.198.117.194", "39248");
+        map.put("117.131.75.134", "80");
+        map.put("121.8.98.196", "80");
+        map.put("221.7.255.168", "8080");
+        map.put("120.76.77.152", "9999");
+
+        map.put("117.87.177.58", "9000");
         map.put("124.94.196.188", "9999");
         map.put("110.52.235.76", "9999");
         map.put("111.177.190.124", "9999");
@@ -164,30 +164,63 @@ public class HttpClient {
     public WebResult getOkhttpHtml(String url) {
         WebResult webResult = new WebResult();
         webResult.setUrl(url);
-        webResult.setCode(200);
+        //webResult.setCode(200);
+        Map.Entry<String, String> entry = getProxy();
+        Response execute = null;
+        ResponseBody body = null;
         try {
             OkHttpClient.Builder builder = new OkHttpClient.Builder();
-            //设置连接超时时间
-            builder.connectTimeout(1, TimeUnit.MINUTES);
+            //设置连接超时时间  --15 Ms
+            builder.connectTimeout(15, TimeUnit.SECONDS);
+
             //设置代理,需要替换
-            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("110.52.235.76", 9999));
-            builder.proxy(proxy);
+            if (entry != null & useProxy) {
+                Proxy proxy = new Proxy(Proxy.Type.HTTP,
+                    new InetSocketAddress(entry.getKey(), Integer.valueOf(entry.getValue())));
+                builder.proxy(proxy);
+            }
+            // header
             Map<String, String> map = new HashMap<>();
             map.put("User-agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
             map.put("Accept-Language", "zh-CN,zh;q=0.8");
             Headers headers = Headers.of(map);
-            Request cookieRequest = new Request.Builder()
+
+            Request request = new Request.Builder()
                 .headers(headers)
                 .url(url)
                 .get()
                 .build();
-            Response execute =  builder.build().newCall(cookieRequest).execute();
-            ResponseBody body = execute.body();
+            execute =  builder.build().newCall(request).execute();
+            body = execute.body();
             //System.out.println(body.string());
             webResult.setResult(body.string());
+            webResult.setCode(200);
             execute.close();
+        } catch (java.net.SocketTimeoutException ste) {
+            webResult.setCode(-1);
+            log.info("this url socket Time out:" + url);
+        } catch (java.net.UnknownHostException e) {
+            webResult.setCode(-2);
+            log.info("this url unknow host:" + url);
         } catch (Exception e) {
-            log.error("okhttpGetHtml error", e);
+            log.error("this url error -->" + url, e);
+        } finally {
+            try {
+                if (body != null) {
+                    body.close();
+                }
+                if (execute != null) {
+                    execute.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+
+            //
+            String proxy = useProxy ? entry.getKey() : "localhost";
+            if (recording) {
+                crawlMapper.insert(url, System.currentTimeMillis(), webResult.getCode(), proxy);
+            }
         }
         return webResult;
     }
@@ -202,7 +235,7 @@ public class HttpClient {
         if (lastRandomNumber != null && lastRandomNumber.equals(a)) {
             getProxy();
         }
-        System.out.println(a);
+        //System.out.println(a);
         // 赋值
         lastRandomNumber = a;
 
