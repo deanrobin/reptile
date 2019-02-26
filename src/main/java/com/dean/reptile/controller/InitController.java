d31deecc934631e2e1c84bc849c855effeaec854
dean <ladeanrobin@163.com>
2019-02-26 14:59:40 +0800

    fix error

--- src/main/java/com/dean/reptile/controller/InitController.java
+++ src/main/java/com/dean/reptile/controller/InitController.java
@@ -5,7 +5,6 @@ import com.dean.reptile.constant.JobEnum;
 import com.dean.reptile.constant.ResponseStatus;
 import com.dean.reptile.constant.TimesCache;
 import com.dean.reptile.service.impl.C5JewelrySpider;
-import com.dean.reptile.task.QuartzClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
